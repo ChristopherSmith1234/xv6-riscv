a4623c5ffbb583b40afea0b3b7a3c336d20e49e9
Christopher Smith <smith239@unlv.nevada.edu>
2025-02-04 09:38:56 +0000

    added hello user program

---
+++ user/hello.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user.h"
+
+int main() {
+    printf("Hello, world. this is Christopher Smith.\n");
+    printf("I am in CS370, Spring 2025, section 1002.\n");
+
+    exit(0);
+}
